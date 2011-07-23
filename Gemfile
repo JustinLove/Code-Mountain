28776d46ce07459b2915b42a26fdca921b9bfeaa
Justin Love <git@JustinLove.name>
2011-07-23 08:10:35 -0500

    bare rails app

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+
+gem 'sqlite3'
+gem 'haml'
+gem 'sass'
+gem 'decent_exposure'
+gem 'activeadmin'
+
+group :test do
+  gem 'rspec'
+  gem 'rspec-given'
+  gem 'factory_girl_rails'
+  gem 'webrat'
+end
+
+group :development do
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codemountain::Application.config.secret_token = '9d07342adf67a1037fc12d650f0baa5c22cc450ac4d23766c38ba595a9cedec0644460c44f0004aa625210f228f4e10bfb73dcb8d1c7c17d4f1884c5f54df874'

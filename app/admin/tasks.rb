627aa4ae229234a9367ad5b550283d40031c3b84
Justin Love <git@JustinLove.name>
2011-07-23 21:03:53 -0500

    admin action to clear tasks

--- app/admin/tasks.rb
+++ app/admin/tasks.rb
@@ -1,7 +1,18 @@
 ActiveAdmin.register Task do
   index do |f|
-    column :id
+    column :id, do |task| 
+      link_to task.id, admin_task_path(task)
+    end
     column :lesson
     column :status
+  end
+
+  action_item(:only => :index) do
+    link_to "Delete All", delete_all_admin_tasks_path
+  end
+
+  collection_action :delete_all do
+    Task.destroy_all
+    redirect_to admin_tasks_path
   end
 end

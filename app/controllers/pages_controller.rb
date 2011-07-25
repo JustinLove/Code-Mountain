419d9c6c4b56eac92844d9d6204af1d8719d715f
Brian Chamberlain <bchamberlain@pathf.com>
2011-07-25 14:16:39 -0500

    Changes to show a task

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   layout 'marketing'
 
+  def dashboard
+    @next_task = Lesson.first.task!
+    @next_task.save!
+  end
+
   def tutorial
     render :layout => 'navless'
   end

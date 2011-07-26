29ab2c6194452fdc7e7d3c57112cf4e7199719a5
Justin Love <git@JustinLove.name>
2011-07-26 13:45:59 -0500

    fade notice out on click

--- public/javascripts/jquery.message_pop.js
+++ public/javascripts/jquery.message_pop.js
@@ -1,17 +1,24 @@
 (function( $ ){
 
   $.fn.pop_message = function() {
-  
+
     // there's no need to do $(this) because
     // "this" is already a jquery object
 
     // $(this) would be the same as $($('#element'));
-        
+
     this.fadeIn('normal', function(){
 
       // the this keyword is a DOM element
 
     });
+
+    var pop = this
+
+    console.log(this.find('a'))
+    this.find('a').click(function() {
+      pop.fadeOut('normal')
+    })
 
   };
 })( jQuery );
